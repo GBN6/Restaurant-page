18c163b2b569aa5f06c630a01f94280bd2304c61
GBN6 <czesiox8@gmail.com>
2022-07-06 16:27:39 +0200

    Create menu page

--- src/main-site.js
+++ src/main-site.js
@@ -1,4 +1,5 @@
 import initializeHome from "./home";
+import initializeMenu from "./menu";
 
 function createHeader()
 {
@@ -22,20 +23,49 @@ function createNavButtons()
     const homeButton = document.createElement('button');
     homeButton.classList.add('nav-btn', 'home-btn');
     homeButton.textContent = 'Home'
+    homeButton.addEventListener("click", (e) => {
+        if (e.target.classList.contains("active")) return;
+        setActiveButton(homeButton);
+        initializeHome();
+    });
 
     const menuButton = document.createElement('button');
     menuButton.classList.add('nav-btn', 'menu-btn');
     menuButton.textContent = 'Menu'
+    menuButton.addEventListener("click", (e) => {
+        if (e.target.classList.contains("active")) return;
+        setActiveButton(menuButton);
+        initializeMenu();
+    });
 
     const contactButton = document.createElement('button');
     contactButton.classList.add('nav-btn', 'contact-btn');
     contactButton.textContent = 'Contact'
+    contactButton.addEventListener("click", (e) => {
+        if (e.target.classList.contains("active")) return;
+        setActiveButton(contactButton);
+    });
+    
 
     nav.appendChild(homeButton);
     nav.appendChild(menuButton);
     nav.appendChild(contactButton);
 
     return nav;
+}
+
+function setActiveButton(button)
+{
+    const buttons = document.querySelectorAll('.nav-btn');
+
+    buttons.forEach((button) => {
+        if (button !== this)
+        {
+            button.classList.remove('active')
+        }
+    });
+
+    button.classList.add('active');
 }
 
 function createMainContent()
---
+++ src/menu.js
@@ -0,0 +1,87 @@
+function createBurgerMenu()
+{
+    const burgerMenu = document.createElement('div')
+    burgerMenu.classList.add('menu');
+
+    burgerMenu.appendChild(
+        createMenuTile(
+            'Hamburger',
+            'Burger Bun, Beef Patty(200g), Lettuce, Tomatoes, Sauce'
+        )
+    );
+
+    burgerMenu.appendChild(
+        createMenuTile(
+            'Cheese Burger',
+            'Burger Bun, Beef Patty(200g),Cheese Slice, Onions, Sauce'
+        )
+    );
+
+    burgerMenu.appendChild(
+        createMenuTile(
+            'Chicken Burger',
+            'Burger Bun, Chicken Filet(200g),Cheese Slice, Lettuce, Sauce'
+        )
+    );
+
+    burgerMenu.appendChild(
+        createMenuTile(
+            'Mexican Burger',
+            'Burger Bun, Beef Patty(200g), Nachos, Cheese Slice, Lettuce, Onions, Hot Sauce'
+        )
+    );
+
+    burgerMenu.appendChild(
+        createMenuTile(
+            'Village Burger',
+            'Grain Burger Bun, Beef Patty(200g), Tomatoes, Cheese Slice, Lettuce, Onions, Pickles, Sauce'
+        )
+    );
+
+    burgerMenu.appendChild(
+        createMenuTile(
+            'French Fries',
+            'French Fries, Salt, Ketchup'
+        )
+    );
+
+    burgerMenu.appendChild(
+        createMenuTile(
+            'Nachos',
+            'Nachos, Cheese, Sauce'
+        )
+    );
+
+    return burgerMenu;
+}
+
+function createMenuTile(name, info)
+{
+    const menuTile = document.createElement('div');
+    menuTile.classList.add('menu-tile');
+
+    const burgerName = document.createElement('h2');
+    burgerName.textContent = name;
+
+    const burgerInfo = document.createElement('p')
+    burgerInfo.textContent = info;
+
+    const burgerImage = document.createElement('img');
+    burgerImage.src = `images/burgers/${name.split(' ').join('').toLowerCase()}.png`;
+    burgerImage.alt = `${name}`;
+
+    menuTile.appendChild(burgerImage);
+    menuTile.appendChild(burgerName);
+    menuTile.appendChild(burgerInfo);
+
+    return menuTile;
+} 
+
+function initializeMenu()
+{
+    const main = document.querySelector('.main')
+    main.textContent = "";
+    main.appendChild(createBurgerMenu());
+}
+
+export default initializeMenu;
